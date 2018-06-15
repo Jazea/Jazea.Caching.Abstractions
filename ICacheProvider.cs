a8e4b11401d6364a73f8ae51e61829a58a754199
jazea <taoxu.wei@163.com>
2018-06-15 11:57:14 +0800

    Initial commit

---
+++ ICacheProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CacheManager.Core;
+
+namespace Jazea.Caching
+{
+    public interface ICacheProvider<TValue>
+    {
+        bool Exists(string key);
+
+        TValue Get(string key);
+
+        void Remove(string key);
+
+        void Add(string key, TValue value, ExpirationMode expiration, TimeSpan timeout);
+    }
+}
